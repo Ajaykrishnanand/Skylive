2435e19e840b92386bc50c075401e95678a0afa1
Ajay12-p <75928188+Ajay12-p@users.noreply.github.com>
2023-02-04 16:46:17 +0530

    arcana added

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
-import React from "react";
-import { Auth } from "@arcana/auth-react";
+import { Auth, useAuth } from "@arcana/auth-react";
+import { AuthProvider } from "@arcana/auth";
+ import { ProvideAuth } from "@arcana/auth-react";
+
+const provider = new AuthProvider("b373797fae6275c96ac63108a0733bf78ac1863f");
+const onLogin=()=>{
+  // external wallet 
+}
 
 function App() {
-  const [theme, setTheme] = React.useState("light");
-  const [externalWallet, showExternalWallet] = React.useState(false);
-  return (
-    <>
 
-         <Auth externalWallet={externalWallet} theme={theme} />
-    </>
-      
+  return (
+    <ProvideAuth provider={provider}>
+|
+
+    <div>
+    
+        <div className="pt-40">
+          <Auth externalWallet={true} theme={"dark"} className ="outline" onLogin={onLogin}/>
+        </div>
+   
+    </div>
+    </ProvideAuth>
   );
 }
 
 export default App;
+
