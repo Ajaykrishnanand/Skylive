c5e917e5ed02413990b615efd9b9bd9fe3862fc8
Ajay12-p <75928188+Ajay12-p@users.noreply.github.com>
2023-02-07 00:17:01 +0530

    Update Dashbord.js

--- client/src/component/pages/Dashbord.js
+++ client/src/component/pages/Dashbord.js
@@ -1,10 +1,12 @@
+ import { Link } from "react-router-dom";
 const Dashbord = () => {
+  
   return (
     <>  
-      <div className="   ">
+      <div className="  grid cols-3  ">
         {/* border-b-2   className=" pt-20  pl-40*/}
+        <div className=" col-span-4   ">
        <img src="https://i.dummyjson.com/data/products/1/3.jpg" className="w-full h-40"></img>
-        <div className=" flex pl-20   ">
           <div >
             <div className="avatar">
               <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
@@ -18,17 +20,20 @@ const Dashbord = () => {
             <p className="text-lg  pl-10 font-bold">@username</p>
           </div>
          
-        </div>
+      
 
         <div className=" flex justify-evenly text-2xl ">
+        <Link to="/dashbord/videos">
+
           <h3 className=" btn bg-cyan-50 text-black  outline-0">videos</h3>
+        </Link>
           <div className="pl-10">
 
           <h3 className=" btn  bg-cyan-50 text-black  outline-0">about</h3>
           </div>
         </div>
         <hr className=" flex  pt-20 " />
-       
+        </div>
       </div>
          <div className="border-r-2 pl-[80rem]"></div>
     </>
