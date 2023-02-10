223f6bebe26f2959c6a458d655a6ac8a57af1f4d
Ajay12-p <75928188+Ajay12-p@users.noreply.github.com>
2023-02-10 17:16:21 +0530

    Update player.js

--- client/src/component/livepeer/player.js
+++ client/src/component/livepeer/player.js
@@ -1,12 +1,13 @@
 import { Player } from "@livepeer/react";
 function PlayerComponent() {
   return (
-    <div className="flex items-center pl-[32rem] ">
+    <div className="flex items-center component 2xl:ml-[19rem] ml-52 mt-24 2xl:w-[60rem]  w-[40rem] h-[26rem]  2xl:h-full 2xl:pt-auto ">
       {" "}
       <Player
         title="Agent 327: Operation Barbershop"
         playbackId="6d7el73r1y12chxr"
         // poster="/images/blender-poster.png"
+      
       />
     </div>
   );
