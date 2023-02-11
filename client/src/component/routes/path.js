5426e5f672b4833019678be2b9bdb400eb495dec
Ajay12-p <75928188+Ajay12-p@users.noreply.github.com>
2023-02-12 00:53:30 +0530

    Update path.js

--- client/src/component/routes/path.js
+++ client/src/component/routes/path.js
@@ -5,15 +5,22 @@ import User from "../superfluid/User";
 import Live from "../livepeer/Live";
 import Player from "../livepeer/player";
 import Uploadcard from "../pages/Uploadcard";
+import Postcard from "../card/Postcard"
+import Home from "../pages/home";
 const Path = () => {
   return (
     <Routes>
+      <Route exact path="/" element={<Home />} />
+
       <Route exact path="/user" element={<User />} />
       <Route exact path="/upload" element={<Upload />} />
       <Route exact path="/player" element={<Player />} />
       <Route exact path="/creater" element={<Creater />} />
       <Route exact path="/uploadcard" element={<Uploadcard />} />
       <Route exact path="/live" element={<Live />} />
+      <Route exact path="/posts" element={<Postcard/>} />
+
+
     </Routes>
   );
 };
