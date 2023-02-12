6dc0b7eb881726e208d6ffb7ebb89e2b1aab28d9
Ajay12-p <75928188+Ajay12-p@users.noreply.github.com>
2023-02-13 00:21:14 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,8 @@ function App() {
   }
   return (
     <Wrapper>
-    <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem] fixed">
+    <div class="isolate bg-white static">
+    <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem] ">
 
 <svg
   className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
@@ -68,7 +69,7 @@ function App() {
           </div>
         </LivepeerConfig>
 
-        
+        </div>
       </div>
     </Wrapper>
     // <ProvideAuth provider={provider}>
