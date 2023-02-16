82dd20cc7eb5bb2e29169a817b665c0a4b88273a
Trymbak Mahant <trymbak0102mahant@gmail.com>
2023-02-16 18:32:10 +0530

    subscription function

--- client/src/component/context/ApplicationContext.js
+++ client/src/component/context/ApplicationContext.js
@@ -178,6 +178,103 @@ const Wrapper = (props) => {
     }
   }
 
+  //////////////////////////// its create flow to user to creater ////////////////////////////////////
+
+  //where the Superfluid logic takes place
+  async function createNewFlow(recipient, flowRate) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+
+    const signer = provider.getSigner();
+
+    const chainId = await window.ethereum.request({ method: "eth_chainId" });
+    const sf = await Framework.create({
+      chainId: Number(chainId),
+      provider: provider,
+    });
+
+    const superSigner = sf.createSigner({ signer: signer });
+
+    console.log(signer);
+    console.log(await superSigner.getAddress());
+    const daix = await sf.loadSuperToken("fDAIx");
+
+    console.log(daix);
+
+    try {
+      const createFlowOperation = daix.createFlow({
+        sender: await superSigner.getAddress(),
+        receiver: recipient,
+        flowRate: flowRate,
+        // userData?: string
+      });
+
+      console.log(createFlowOperation);
+      console.log("Creating your stream...");
+
+      const result = await createFlowOperation.exec(superSigner);
+      console.log(result);
+
+      console.log(
+        `Congrats - you've just created a money stream!
+    `
+      );
+    } catch (error) {
+      console.log(
+        "Hmmm, your transaction threw an error. Make sure that this stream does not already exist, and that you've entered a valid Ethereum address!"
+      );
+      console.error(error);
+    }
+  }
+
+  //// this  delete the flow btween user and creater
+
+  //where the Superfluid logic takes place
+  async function deleteExistingFlow(recipient) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+
+    const signer = provider.getSigner();
+
+    const chainId = await window.ethereum.request({ method: "eth_chainId" });
+    const sf = await Framework.create({
+      chainId: Number(chainId),
+      provider: provider,
+    });
+
+    const superSigner = sf.createSigner({ signer: signer });
+
+    console.log(signer);
+    console.log(await superSigner.getAddress());
+    const daix = await sf.loadSuperToken("fDAIx");
+
+    console.log(daix);
+
+    try {
+      const deleteFlowOperation = daix.deleteFlow({
+        sender: await signer.getAddress(),
+        receiver: recipient,
+        // userData?: string
+      });
+
+      console.log(deleteFlowOperation);
+      console.log("Deleting your stream...");
+
+      const result = await deleteFlowOperation.exec(superSigner);
+      console.log(result);
+
+      console.log(
+        `Congrats - you've just updated a money stream!
+    `
+      );
+    } catch (error) {
+      console.log(
+        "Hmmm, your transaction threw an error. Make sure that this stream does not already exist, and that you've entered a valid Ethereum address!"
+      );
+      console.error(error);
+    }
+  }
+
   const sharedState = {
     setData,
     channel,
@@ -185,9 +282,11 @@ const Wrapper = (props) => {
     acclogin,
     sidebar,
     setSidebar,
+    createNewFlow,
     upgradeTokens,
     approveTokens,
     downgradeTokens,
+    deleteExistingFlow,
   };
 
   console.log(acclogin);
