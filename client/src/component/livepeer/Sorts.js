6c4891390092dbfa0e4b77d2df26d76753ac200c
Ajay12-p <75928188+Ajay12-p@users.noreply.github.com>
2023-02-19 00:15:11 +0530

    Update Sorts.js

--- client/src/component/livepeer/Sorts.js
+++ client/src/component/livepeer/Sorts.js
@@ -1,7 +1,7 @@
 import { Player } from "@livepeer/react";
 function PlayerComponent() {
   return (
-    <div className="flex items-center component 2xl:ml-[24rem] ml-52 mt-24 2xl:w-[18rem]  w-[27rem] h-[16rem]  2xl:h-[48rem] 2xl:pt-auto ">
+    <div className="flex items-center component 2xl:ml-[24rem] 2xl:h-[48rem] ml-50 mt-24 2xl:w-[18rem]  w-[27rem] h-[16rem]   rounded-xl 2xl:pt-auto ">
       {" "}
       <Player
         title="Agent 327: Operation Barbershop"
