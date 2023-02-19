a3b6ef35ddbcbae69c26a14057d5fe0077fe24a0
Ajay12-p <75928188+Ajay12-p@users.noreply.github.com>
2023-02-20 01:18:16 +0530

    Update home.js

--- client/src/component/pages/home.js
+++ client/src/component/pages/home.js
@@ -2,20 +2,7 @@ import Postcard from "../card/Postcard";
 import axios from "axios";
 import { useEffect, useState } from "react";
 const Home=()=>{
-  // const[posts,setPost]= useState([]);
-  // const getPosts=async()=>{
-  //   console.log("hello form the home ..")
-  //   try{
-  //     const{data} = await axios.get("https://dummyjson.com/products")
-  //     setPost(data);
-  //     console.log(posts.products)
-  //   }catch(e){
-  //     console.log(e)
-  //   }
-  // }
-  // useEffect(()=>{
-  //   getPosts()
-  // },[]) 
+ 
     return(
         <>
         
